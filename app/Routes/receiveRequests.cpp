0c79508c80d7b8dcbb5bdb7f92821457369e0975
Rodrigo Estrela <160602751+estrela801@users.noreply.github.com>
2025-10-08 18:40:37 +0000

    Criando uma rota POST

--- app/Routes/receiveRequests.cpp
+++ app/Routes/receiveRequests.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-
+#include 
 using namespace std;
 
 void setupMessages(crow::SimpleApp& app)
 {
-    CROW_ROUTE(app, "/receive")([]()
+    CROW_ROUTE(app, "/receive").methods("POST"_method)(
 {
-    
-})
+    return ReceiveController::verifyReceive(request );
+});
 }
