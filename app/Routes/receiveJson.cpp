eb9a43c6f1aca0fdb29848ff41f2e7fd98c847f3
Rodrigo Estrela <160602751+estrela801@users.noreply.github.com>
2025-11-11 16:54:41 +0000

    Atualizando nomes de arquivos

--- app/Routes/receiveJson.cpp
+++ app/Routes/receiveJson.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#include 
+#include <"crow.h">
+#include "app/Controllers/receiveController.cpp"
 using namespace std;
 
 void setupMessages(crow::SimpleApp& app)
 {
     CROW_ROUTE(app, "/receive").methods("POST"_method)(
 {
-    return ReceiveController::verifyReceive(request );
+    return ReceiveController::verifyReceive(request);
 });
 }
